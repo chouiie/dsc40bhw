ff412a896c0a6757a0c59a71b7e5e2212d09a137
chouiie <135649305+chouiie@users.noreply.github.com>
2026-05-20 18:32:11 -0700

    Update cluster.py

---
+++ cluster.py
@@ -0,0 +1,26 @@
+def cluster(graph, weights, level):
+    visited = set()
+    clusters = set()
+
+    for node in graph.nodes():
+
+        if node not in visited:
+
+            component = set()
+            stack = [node]
+            visited.add(node)
+
+            while stack:
+                current = stack.pop()
+                component.add(current)
+
+                for neighbor in graph.neighbors(current):
+
+                    if neighbor not in visited:
+                        if weights(current, neighbor) >= level:
+                            visited.add(neighbor)
+                            stack.append(neighbor)
+
+            clusters.add(frozenset(component))
+
+    return frozenset(clusters)
